dcb3bf876c5078b0c7c4e662ce748d6a39e479e4
abubdcalling <abubdcalling@gmail.com>
2025-06-02 13:18:08 +0600

    add

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -8,21 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Content extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'category_id',
-        'subcategory_id',
-        'heading',
-        'credits',
-        'date',
-        'sub_heading',
-        'body',
-        'image_1',
-        'advertising',
-        'tags',
-    ];
-
-    protected $casts = [
-        'tags' => 'array',
-    ];
 }
