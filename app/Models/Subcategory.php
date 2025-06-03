0e4e59ffbd8d4c2fae43572f1669484c37765439
abubdcalling <abubdcalling@gmail.com>
2025-06-03 14:23:20 +0600

    add

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -1,13 +1,15 @@
-<?php
+
+
+<?php 
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SubCategory extends Model
+class SubCategory extends Model 
 {
     use HasFactory;
+
     protected $fillable = ['category_id', 'name'];
 
     public function category()
