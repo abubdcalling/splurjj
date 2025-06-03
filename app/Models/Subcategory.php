fab8c34551c7b25d9b6cfb6fb36de295e0fceec7
abubdcalling <abubdcalling@gmail.com>
2025-06-03 14:25:53 +0600

    add

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -1,11 +1,16 @@
 
 
-<?php 
 
+
+
+
+
+
+
+<?php 
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class SubCategory extends Model 
 {
     use HasFactory;
